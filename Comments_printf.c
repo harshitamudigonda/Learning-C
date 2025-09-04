c20f7a71edab6eb1617366e50be345f57bbc4eed
Harshita Mudigonda <harshitamudigonda@gmail.com>
2025-09-04 14:29:39 +0530

    Comments and printf

---
+++ Comments_printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+  //First code in C
+  /*
+  I really love programming
+  I want to learn C*/
+
+  printf("I like pizza!\n");
+  printf("It really tastes good.");
+}
